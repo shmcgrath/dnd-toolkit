66119f42cd100d65dbdce4afeabcc2456627fdd7
Sarah H. McGrath <sarah@shmcgrath.com>
2020-03-21 14:01:09 -0400

    Add Templates, chars variable
    End of The Mega Flask Tutorial Part II

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, adventurer!"
+    user = {'username': 'Sarah'}
+    chars = [
+        {
+            'owner': {'username': 'Mo'},
+            'body': 'Level 3 Monk'
+        },
+        {
+            'owner': {'username': 'Jon'},
+            'body': 'Level 10 Cleric'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, chars=chars)
